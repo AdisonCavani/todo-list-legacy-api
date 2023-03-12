60124c103fa9a918e8741587811556ee111242e7
Adison Cavani <adriansrodon@tuta.io>
2023-03-12 21:39:04 +0100

    Revert "Frontend | PWA theme-color fix" 3

--- frontend/components/theme-provider.tsx
+++ frontend/components/theme-provider.tsx
@@ -11,6 +11,7 @@ function NextThemeProvider({ children }: PropsWithChildren) {
 
   return (
     <ThemeProvider
+      attribute="class"
       defaultTheme="system"
       enableSystem
       enableColorScheme

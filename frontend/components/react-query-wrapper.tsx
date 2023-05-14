1cfb66e0a0167bc31d525f1edf2ef8d0df53db50
Adison Cavani <adriansrodon@tuta.io>
2023-05-14 21:18:48 +0200

    Frontend | Disable refetchOnWindowFocus for react-query
    This will reduce unnecessary API request and save $$$

--- frontend/components/react-query-wrapper.tsx
+++ frontend/components/react-query-wrapper.tsx
@@ -4,7 +4,16 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren, useState } from "react";
 
 function ReactQueryWrapper({ children }: PropsWithChildren) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  );
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>

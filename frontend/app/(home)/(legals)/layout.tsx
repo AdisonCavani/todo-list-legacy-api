dd6d50e9679451612db53b7da35de3b1f38b8b26
Adison Cavani <adriansrodon@tuta.io>
2023-06-02 23:58:02 +0200

    Frontend | Switch to dynamic in (legals) segment
    Login button was not rendering correctly due to SSG

--- frontend/app/(home)/(legals)/layout.tsx
+++ frontend/app/(home)/(legals)/layout.tsx
@@ -1,7 +1,5 @@
 import { PropsWithChildren } from "react";
 
-export const dynamic = "force-static";
-
 function Layout({ children }: PropsWithChildren) {
   return (
     <main className="prose mx-auto my-14 max-w-5xl px-8 dark:prose-invert">

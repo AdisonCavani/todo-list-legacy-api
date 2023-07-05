edcae08b73752c06cf7ce100f11769c4d2c8e7f4
Adison Cavani <adriansrodon@tuta.io>
2023-07-05 20:25:28 +0200

    fix: sql schema naming convention

--- frontend/db/migrations/0000_mixed_robbie_robertson.sql
+++ frontend/db/migrations/0000_mixed_robbie_robertson.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `accounts` (
 	`id` varchar(255) PRIMARY KEY NOT NULL,
-	`userId` varchar(255) NOT NULL,
+	`user_id` varchar(255) NOT NULL,
 	`type` varchar(255) NOT NULL,
 	`provider` varchar(255) NOT NULL,
-	`providerAccountId` varchar(255) NOT NULL,
+	`provider_account_id` varchar(255) NOT NULL,
 	`access_token` text,
 	`expires_in` int,
 	`id_token` text,
@@ -11,22 +11,22 @@ CREATE TABLE `accounts` (
 	`refresh_token_expires_in` int,
 	`scope` varchar(255),
 	`token_type` varchar(255),
-	`createdAt` timestamp NOT NULL DEFAULT (now()),
-	`updatedAt` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP);
+	`created_at` timestamp NOT NULL DEFAULT (now()),
+	`updated_at` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP);
 --> statement-breakpoint
 CREATE TABLE `sessions` (
 	`id` varchar(255) PRIMARY KEY NOT NULL,
-	`sessionToken` varchar(255) NOT NULL,
-	`userId` varchar(255) NOT NULL,
+	`session_token` varchar(255) NOT NULL,
+	`user_id` varchar(255) NOT NULL,
 	`expires` datetime NOT NULL,
 	`created_at` timestamp NOT NULL DEFAULT (now()),
 	`updated_at` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP);
 --> statement-breakpoint
 CREATE TABLE `tasks` (
 	`id` varchar(255) PRIMARY KEY NOT NULL,
-	`userId` varchar(255) NOT NULL,
-	`createdAt` timestamp NOT NULL DEFAULT (now()),
-	`updatedAt` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
+	`user_id` varchar(255) NOT NULL,
+	`created_at` timestamp NOT NULL DEFAULT (now()),
+	`updated_at` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
 	`title` text NOT NULL,
 	`description` text,
 	`due_date` date,
@@ -39,7 +39,7 @@ CREATE TABLE `users` (
 	`first_name` varchar(255) NOT NULL,
 	`last_name` varchar(255) NOT NULL,
 	`email` varchar(255) NOT NULL,
-	`emailVerified` timestamp,
+	`email_verified` timestamp,
 	`image` varchar(255),
 	`created_at` timestamp NOT NULL DEFAULT (now()),
 	`updated_at` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP);
@@ -51,9 +51,9 @@ CREATE TABLE `verification_tokens` (
 	`created_at` timestamp NOT NULL DEFAULT (now()),
 	`updated_at` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP);
 --> statement-breakpoint
-CREATE UNIQUE INDEX `accounts__provider__providerAccountId__idx` ON `accounts` (`provider`,`providerAccountId`);--> statement-breakpoint
-CREATE INDEX `accounts__userId__idx` ON `accounts` (`userId`);--> statement-breakpoint
-CREATE UNIQUE INDEX `sessions__sessionToken__idx` ON `sessions` (`sessionToken`);--> statement-breakpoint
-CREATE INDEX `sessions__userId__idx` ON `sessions` (`userId`);--> statement-breakpoint
+CREATE UNIQUE INDEX `accounts__provider__providerAccountId__idx` ON `accounts` (`provider`,`provider_account_id`);--> statement-breakpoint
+CREATE INDEX `accounts__userId__idx` ON `accounts` (`user_id`);--> statement-breakpoint
+CREATE UNIQUE INDEX `sessions__sessionToken__idx` ON `sessions` (`session_token`);--> statement-breakpoint
+CREATE INDEX `sessions__userId__idx` ON `sessions` (`user_id`);--> statement-breakpoint
 CREATE UNIQUE INDEX `users__email__idx` ON `users` (`email`);--> statement-breakpoint
 CREATE UNIQUE INDEX `verification_tokens__token__idx` ON `verification_tokens` (`token`);
--- frontend/db/schema.ts
+++ frontend/db/schema.ts
@@ -15,9 +15,9 @@ import {
 
 export const tasks = mysqlTable("tasks", {
   id: varchar("id", { length: 255 }).primaryKey().notNull(),
-  userId: varchar("userId", { length: 255 }).notNull(),
-  createdAt: timestamp("createdAt").defaultNow().notNull(),
-  updatedAt: timestamp("updatedAt").defaultNow().onUpdateNow().notNull(),
+  userId: varchar("user_id", { length: 255 }).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().onUpdateNow().notNull(),
   title: text("title").notNull(),
   description: text("description"),
   dueDate: date("due_date"),
@@ -32,10 +32,12 @@ export const accounts = mysqlTable(
   "accounts",
   {
     id: varchar("id", { length: 255 }).primaryKey().notNull(),
-    userId: varchar("userId", { length: 255 }).notNull(),
+    userId: varchar("user_id", { length: 255 }).notNull(),
     type: varchar("type", { length: 255 }).notNull(),
     provider: varchar("provider", { length: 255 }).notNull(),
-    providerAccountId: varchar("providerAccountId", { length: 255 }).notNull(),
+    providerAccountId: varchar("provider_account_id", {
+      length: 255,
+    }).notNull(),
     access_token: text("access_token"),
     expires_in: int("expires_in"),
     id_token: text("id_token"),
@@ -43,8 +45,8 @@ export const accounts = mysqlTable(
     refresh_token_expires_in: int("refresh_token_expires_in"),
     scope: varchar("scope", { length: 255 }),
     token_type: varchar("token_type", { length: 255 }),
-    createdAt: timestamp("createdAt").defaultNow().notNull(),
-    updatedAt: timestamp("updatedAt").defaultNow().onUpdateNow().notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().onUpdateNow().notNull(),
   },
   (account) => ({
     providerProviderAccountIdIndex: uniqueIndex(
@@ -58,8 +60,8 @@ export const sessions = mysqlTable(
   "sessions",
   {
     id: varchar("id", { length: 255 }).primaryKey().notNull(),
-    sessionToken: varchar("sessionToken", { length: 255 }).notNull(),
-    userId: varchar("userId", { length: 255 }).notNull(),
+    sessionToken: varchar("session_token", { length: 255 }).notNull(),
+    userId: varchar("user_id", { length: 255 }).notNull(),
     expires: datetime("expires").notNull(),
     created_at: timestamp("created_at").notNull().defaultNow(),
     updated_at: timestamp("updated_at").notNull().defaultNow().onUpdateNow(),
@@ -79,7 +81,7 @@ export const users = mysqlTable(
     firstName: varchar("first_name", { length: 255 }).notNull(),
     lastName: varchar("last_name", { length: 255 }).notNull(),
     email: varchar("email", { length: 255 }).notNull(),
-    emailVerified: timestamp("emailVerified"),
+    emailVerified: timestamp("email_verified"),
     image: varchar("image", { length: 255 }),
     created_at: timestamp("created_at").notNull().defaultNow(),
     updated_at: timestamp("updated_at").notNull().defaultNow().onUpdateNow(),

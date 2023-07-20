9f8ae59dc8255b442a438e6faa2026a8eb231b57
Adison Cavani <adriansrodon@tuta.io>
2023-07-20 21:02:00 +0200

    feat: add localStorage storage persister (offline mode)

--- frontend/components/react-query-wrapper.tsx
+++ frontend/components/react-query-wrapper.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+import { QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import { useState, type PropsWithChildren } from "react";
 
 function ReactQueryWrapper({ children }: PropsWithChildren) {
@@ -10,13 +13,34 @@ function ReactQueryWrapper({ children }: PropsWithChildren) {
         defaultOptions: {
           queries: {
             refetchOnWindowFocus: false,
+            gcTime: 1000 * 60 * 60 * 24, // 24 hours
+            staleTime: 2000,
+            retry: 0,
           },
         },
       }),
   );
 
+  const [persister] = useState(() =>
+    createSyncStoragePersister({
+      storage: typeof window !== "undefined" ? window.localStorage : undefined,
+    }),
+  );
+
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister: persister }}
+      onSuccess={() => {
+        // resume mutations after initial restore from localStorage was successful
+        queryClient.resumePausedMutations().then(() => {
+          queryClient.invalidateQueries();
+        });
+      }}
+    >
+      {children}
+      <ReactQueryDevtools initialIsOpen />
+    </PersistQueryClientProvider>
   );
 }
 

136f27bbd2e2728f2f0585158e5b34df678544fe
Adison Cavani <adriansrodon@tuta.io>
2023-09-14 22:41:23 +0200

    style: fix sidenav css in /app

--- src/app/app/(view)/layout.tsx
+++ src/app/app/(view)/layout.tsx
@@ -18,7 +18,7 @@ async function Layout({ children }: PropsWithChildren) {
     <main className="flex grow flex-row">
       <AuthWrapper>
         <ReactQueryWrapper>
-          <nav className="sticky top-[72px] hidden h-full w-72 flex-col gap-y-3 border-r bg-secondary px-4 py-7 lg:flex">
+          <nav className="sticky flex w-72 flex-col gap-y-3 border-r bg-secondary px-4 py-7">
             <SideNav initialLists={response} />
           </nav>
           <section className="flex w-full grow flex-col px-3 pb-24 pt-8 sm:px-6 sm:pb-8">

f9192e00fef131427b3363ce903cc507fce399bd
Adison Cavani <adriansrodon@tuta.io>
2023-09-18 22:59:03 +0200

    style: remove unused css in profile-menu component

--- src/components/app/profile-menu.tsx
+++ src/components/app/profile-menu.tsx
@@ -34,7 +34,7 @@ function ProfileMenu({ firstName, lastName, email, image }: User) {
       <DropdownMenuTrigger asChild>
         <Button
           variant="ghost"
-          className="relative h-8 w-8 rounded-full hover:bg-transparent"
+          className="h-8 w-8 rounded-full hover:bg-transparent"
         >
           <Avatar>
             <AvatarImage src={image ?? undefined} alt="User avatar" />

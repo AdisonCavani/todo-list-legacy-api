70cd60a6d30605a0ef63b51eb22aef8724240d02
Adison Cavani <adriansrodon@tuta.io>
2023-09-18 23:01:08 +0200

    fix: /app/[id] - return 404 if list does not exist

--- src/app/app/(view)/[id]/page.tsx
+++ src/app/app/(view)/[id]/page.tsx
@@ -1,16 +1,28 @@
 import App from "@components/app/app";
-import { tasks } from "@db/schema";
+import { lists, tasks } from "@db/schema";
 import { db } from "@db/sql";
-import { eq } from "drizzle-orm";
+import { auth } from "@lib/auth";
+import { and, eq } from "drizzle-orm";
+import { notFound } from "next/navigation";
 
 type Props = {
   params: { id: string };
 };
 
 async function Page({ params: { id } }: Props) {
-  const response = await db.select().from(tasks).where(eq(tasks.listId, id));
+  const session = await auth();
 
-  return <App initialTasks={response} listId={id} />;
+  const [listsResponse, tasksResponse] = await Promise.all([
+    db
+      .selectDistinct()
+      .from(lists)
+      .where(and(eq(lists.id, id), eq(lists.userId, session.user.id))),
+    db.select().from(tasks).where(eq(tasks.listId, id)),
+  ]);
+
+  if (!listsResponse.length) notFound();
+
+  return <App initialTasks={tasksResponse} listId={id} />;
 }
 
 export default Page;
